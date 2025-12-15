f4bddc176c00f55594d51a52b514de4e5d5edb90
Ryuguuu <130326057+Ryuguuu@users.noreply.github.com>
2025-12-15 09:54:22 +0900

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2032451197"
+    serviceWorkerVersion: "3393926313"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cdfe6c1e6442575a11482b3124e294c4",
+const RESOURCES = {"flutter_bootstrap.js": "3c1829be391e0a863cb71574f40062ff",
 "version.json": "4a85b6de5d73c5e8d65922fb87b9224d",
 "index.html": "d0ac24b9b63ef58c9891abda88cace13",
 "/": "d0ac24b9b63ef58c9891abda88cace13",
-"main.dart.js": "36f1a35700893dee4026b1ae9ce6db15",
+"main.dart.js": "a34c11faa0c7bab78e54529df5c9025b",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
